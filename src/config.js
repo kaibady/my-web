2e92a134af0def9f2e51453c236f7ef2e31d03f0
chenhauchun <samchen08@163.com>
2020-07-10 11:25:04 +0800

    支持应用微服务

--- src/config.js
+++ src/config.js
@@ -2,37 +2,43 @@ import {set as setConfig} from '$ui/config'
 
 // 写入运行时配置
 setConfig({
-  
-  // 路由模式
+
+  /**
+   * 路由模式
+   */
   router: {
     mode: 'hash',
     base: '/'
   },
-  
-  // 是否应用自动生成的路由配置
+
+  /**
+   * 是否应用自动生成的路由配置
+   */
   autoRoutes: true,
-  
+
+  /**
+   * 权限控制配置
+   */
   access: {
     // 启用请求权限控制
     axios: true,
-    
+
     // 启用路由权限控制
     router: true,
-    
+
     // 缓存存储方式 session 或 local
     storage: 'session',
-    
+
     // 登录页面路径
     loginPath: '/login',
-    
+
     // 权限不足页面路径
     authorizePath: '/403'
   },
-  mock: {
-    timeout: '200-500'
-  },
-  axios: {
-    withCredentials: true
-  }
+
+  /**
+   * 开启访问分析追踪
+   */
+  analysis: true
 })
 
